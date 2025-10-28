697ad3b93497eac152e0167505189a6766ec68c3
M-TALHA  BABAR <babarmtalha91@gmail.com>
2025-10-28 14:29:47 +0500

    bbb

--- src/lib/asyncStorageShim.ts
+++ src/lib/asyncStorageShim.ts
@@ -3,7 +3,6 @@ const AsyncStorage = {
   getItem: async (_key: string) => null,
   setItem: async (_key: string, _value: string) => {},
   removeItem: async (_key: string) => {},
-  clear: async () => {},
 };
 
 export default AsyncStorage;
