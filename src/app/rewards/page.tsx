57a347818b716b364e3f633bd45a82a3fa4b5d5f
M-TALHA  BABAR <babarmtalha91@gmail.com>
2025-10-31 15:38:14 +0500

    BCBV

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -6,7 +6,7 @@ import {
   FaGem,
   FaAward,
   FaMobile,
-  FaCar,
+  FaCar,                                                                                          
   FaMotorcycle,
   FaHome,
   FaLock,
