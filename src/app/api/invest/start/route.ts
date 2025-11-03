c54d41dd238cce1645ad0264edc349f301810195
M-TALHA  BABAR <babarmtalha91@gmail.com>
2025-11-04 02:02:59 +0500

    errors fix

--- src/app/api/invest/start/route.ts
+++ src/app/api/invest/start/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
-import { Deposit } from "@/models/Deposit";
+import  Deposit  from "@/models/Deposit";
 import Investment from "@/models/Investment";
 import { Withdrawal } from "@/models/Withdrawal";
 import { calculateDailyRate } from "@/lib/staking";
